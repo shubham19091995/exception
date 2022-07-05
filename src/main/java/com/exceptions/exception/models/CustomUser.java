7a6ec9cd42573050823ba35ef497c8669ed9d863
shubham871234 <106596578+shubham871234@users.noreply.github.com>
2022-07-05 20:39:52 +0530

    new Changes

--- src/main/java/com/exceptions/exception/models/CustomUser.java
+++ src/main/java/com/exceptions/exception/models/CustomUser.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.GeneratorType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
